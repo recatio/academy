341a689e627c5613f47c833b6f4120221c2d11c6
soohyun <136429438+recatio@users.noreply.github.com>
2023-08-10 15:14:37 +0900

    Update ItemDrag.cs

--- practice/item/ItemDrag.cs
+++ practice/item/ItemDrag.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ItemDrag : ItemProperty, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class ItemDrag : ItemProperty, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
 
     public Transform orgItemSlotTf;
@@ -27,25 +27,9 @@ public class ItemDrag : ItemProperty, IBeginDragHandler, IDragHandler, IEndDragH
         itemIcon.transform.position = orgItemSlotTf.position;
     }
 
-    public void OnDrop()
+    public void OnDrop(PointerEventData eventData)
     {
         itemIcon.SetActive(false);
-        itemIcon.transform.localPosition = transform.position;
-    }
-
-    // 그밖에 필요한 부분
-    // HUD (퀵슬롯)를 호출 버튼을 통해 하단에 별도로 SetActive 되도록 설정하고
-    // 역시 HUD에도 Inventory 아이템을 드래그해서 슬롯에 넣으면 등록될 수 있도록 이벤트 설정.
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        itemIcon.transform.localPosition = eventData.position;
     }
 }
